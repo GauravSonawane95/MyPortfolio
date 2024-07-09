932398431025adaf8ff6b71eb260efb808d99e17
Gaurav_Git <sonawanegs95@gmail.com>
2024-07-09 17:04:41 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,5 @@ export class AppComponent {
   title = 'myport';
   Name:string="Gaurav Sonawane";
   Designation:string="Angular Developer/Front End Developer"
+  about='Fontend Developer with a strong foundation in JavaScript, TypeScript, HTML5, CSS3, Bootstrap, Angular Material, and the Angular framework. Known for creating seamless and visually appealing web experiences, As a recent MCA graduate, I am well-versed in the latest frontend development technologies and trends.'
 }
