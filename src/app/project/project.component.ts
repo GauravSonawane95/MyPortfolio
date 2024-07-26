ce7ff2c926c1ab75a9f2c27ba3df58460205ec43
Gaurav_Git <sonawanegs95@gmail.com>
2024-07-26 19:49:06 +0530

    projects Update

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -7,4 +7,28 @@ import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angula
 })
 export class ProjectComponent  {
 
+  projects:any=[
+    {
+      Name:"Car Insurance",
+      Description:"A web-based car insurance management system using Angular, HTML, CSS, and Bootstrap.Designing and implementing the user interface to create a responsive and visually appealing application. Assisted in developing reusable components and services for data retrieval, validation, and manipulation, ensuring code organization and maintainability. Worked on integrating the application with backend APIs and implementing basic authentication features. gained practical experience in using Angular, Typescript, JavaScript, HTML, CSS, Bootstrap, Git.",
+      Dev:"Developed in Angular"
+  }, {
+    Name:"BikePickers",
+    Description:"A website that helps user to get more information about the bike which they're going to buy.",
+    Dev:" Developed using HTML5,CSS,JavaScript"
+}, {
+  Name:"AI Assisant Using Google Generative AI SDK",
+  Description:"An ai assistant build using Google generative AI SDK that act like an your personal Assistant."
+}, {
+  Name:"Web Portal For an Instiute",
+  Description:"Designed a portal to help institutes sell their products online for insuring more profit and reduced manual work.",
+  Dev:"Developed in ASP.net"
+},
+{
+  Name:"Nameless Helper",
+  Description:"A website for people who want to donate but do not want to disclose their identity.",
+  Dev:"Developed using HTML5,CSS,JavaScript"
+},
+]
+
 }
